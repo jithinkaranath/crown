8efe0a6991715707708794ee4a82bc17b3fc5138
Jithin Karanath <jithinks@ikm.gov.in>
2025-05-31 12:06:00 +0530

    fix: :bug: overrided equals and hashcode

--- src/main/java/uk/co/prodapt/inventory/model/Product.java
+++ src/main/java/uk/co/prodapt/inventory/model/Product.java
@@ -55,4 +55,50 @@ public class Product {
     public void setSupplierName(String supplierName) {
         this.supplierName = supplierName;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + (available ? 1231 : 1237);
+        result = prime * result + ((supplierId == null) ? 0 : supplierId.hashCode());
+        result = prime * result + ((supplierName == null) ? 0 : supplierName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (available != other.available)
+            return false;
+        if (supplierId == null) {
+            if (other.supplierId != null)
+                return false;
+        } else if (!supplierId.equals(other.supplierId))
+            return false;
+        if (supplierName == null) {
+            if (other.supplierName != null)
+                return false;
+        } else if (!supplierName.equals(other.supplierName))
+            return false;
+        return true;
+    }
 }
